200681ea6bc516afadd026a901b75a1d116d3573
Mubasshir Ahin <mubasshirahinpa@gmail.com>
2026-07-03 21:14:10 +0600

    fix: stop cursor loading on app entry

--- src/utils/notificationService.js
+++ src/utils/notificationService.js
@@ -72,8 +72,8 @@ export function sendNotification(title, body, opts = {}) {
   try {
     const n = new Notification(title, {
       body,
-      icon: '/favicon.ico',
-      badge: '/favicon.ico',
+      icon: '/favicon.svg',
+      badge: '/favicon.svg',
       tag: opts.tag || title,
       requireInteraction: opts.requireInteraction || false,
       ...opts,
